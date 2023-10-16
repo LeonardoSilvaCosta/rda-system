fc34609b1b99975b495535753c19df4ce8ee52d1
Leonardo Costa <leonardocostapsi@gmail.com>
2023-10-16 19:04:33 -0300

    Adiciona export const dynamic = 'force-dynamic' em api routes

--- src/app/api/get_attendeds/route.ts
+++ src/app/api/get_attendeds/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 
+export const dynamic = 'force-dynamic';
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(req: NextRequest) {
   const supabase = createRouteHandlerClient({ cookies });
