b1def082bce2587d1255fb0278270811dd0e3edd
Leonardo Costa <leonardocostapsi@gmail.com>
2023-12-14 21:11:42 -0300

    mapeia o default value com novos valores em address

--- src/app/Record/page.tsx
+++ src/app/Record/page.tsx
@@ -36,7 +36,11 @@ export default function Record() {
       number: '',
       street: '',
       complement: '',
-      neighborhood: ''
+      neighborhood: '',
+      city: '',
+      city_id: '',
+      state_id: '',
+      stateAcronym: ''
     },
     phones: [],
     policyHolder: {
