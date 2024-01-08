3a55bf2b5c1db36a2f56d21372bedcb02490573d
Leonardo Costa <leonardocostapsi@gmail.com>
2024-01-08 09:31:45 -0300

    retorna para /redefinePassword

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,7 +20,7 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
-  const allowedPaths = ['/login', '/forgotPassword', '/redefinePassword/*'];
+  const allowedPaths = ['/login', '/forgotPassword', '/redefinePassword'];
   const isAllowed = allowedPaths.some((allowedPath) =>
     pathname.startsWith(allowedPath)
   );
